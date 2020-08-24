61353d679463eb6b22f98c1b138953ede079daa6
Nick Ray <nickrray@gmail.com>
2020-08-24 16:00:14 -0500

    :sparkles: Create database client

---
+++ server/db/client.js
@@ -0,0 +1,25 @@
+import dotenv from 'dotenv'
+import { MongoClient } from 'mongodb'
+
+dotenv.config()
+
+const client = new MongoClient(
+  process.env.MONGODB_URI,
+  {
+    useUnifiedTopology: true
+  }
+);
+
+(async () => {
+  client.connect()
+
+  process.on('SIGINT', () => {
+    client.close().then(() => {
+      console.info('SIGINT received: DB connection is closing')
+
+      process.exit(0)
+    })
+  })
+
+})()
+
