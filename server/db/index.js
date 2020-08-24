fcd76ba27a5f54d44ef52f95afc97aa09de9c3a7
Nick Ray <nickrray@gmail.com>
2020-08-24 17:44:43 -0500

    :rotating_light: Add semicolons to silence airbnb styling

--- server/db/index.js
+++ server/db/index.js
@@ -1,17 +1,17 @@
-import client from './client'
+import client from './client';
 
 export const addTravel = async (newTravel) => {
   try {
-    return await client.db('travelplans').collection('travels').insertOne(newTravel)
+    return await client.db('travelplans').collection('travels').insertOne(newTravel);
   } catch (err) {
-    throw new Error(err)
+    throw new Error(err);
   }
-}
+};
 
 export const deleteTravels = async () => {
   try {
     return await client.db('travelplans').collection('travels').deleteMany({});
   } catch (err) {
-    throw new Error(err)
+    throw new Error(err);
   }
-}
+};
