0607def9eb396e61ded7e2ad9631e229deddbb81
Nick Ray <nickrray@gmail.com>
2020-08-24 13:32:05 -0500

    :lipstick: Style Header with Bulma classes

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,8 @@
 import React from 'react'
 
-export const Header = () => <h1
-  className="title has-text-white is-size-1">
-  Travel Form
+export const Header = () => <div className="box has-background-link">
+  <h1
+    className="title has-text-white is-size-1">
+    Travel Form
   </h1>
+</div>
