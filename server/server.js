f35e245372c27fab784db0fcbce2d55dcf066101
Nick Ray <nickrray@gmail.com>
2020-08-24 16:46:04 -0500

    :sparkles: Create server, use travel routes and listen on port 5000

---
+++ server/server.js
@@ -0,0 +1,13 @@
+import cors from 'cors'
+import express from 'express'
+import travels from './routes/travels'
+
+const app = express()
+const PORT = 5000
+
+app.use(cors())
+app.use(express.json())
+
+app.use('/travels', travels)
+
+app.listen(PORT, () => console.log(`Server running on Port: ${PORT}`))
