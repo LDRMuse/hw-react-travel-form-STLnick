f257d128ac5665d0dcdc6756ac8fd3f99365cd0a
Nick Ray <nickrray@gmail.com>
2020-08-25 10:09:01 -0500

    :sparkles: Utilize useLocation to conditionally render the proper Link in Header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,8 +1,19 @@
 import React from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
-export const Header = () => <div className="box has-background-link">
-  <h1
-    className="title has-text-white is-size-1">
-    Travel Form
-  </h1>
-</div>
+export const Header = () => {
+  const location = useLocation()
+
+  return (
+    <div className="box has-background-link">
+      <h1
+        className="title has-text-white is-size-1">
+        Travel Form
+    </h1>
+      {location.pathname === '/'
+        ? <Link className='button' to='/login'>Login</Link>
+        : <Link className='button' to='/'>Home</Link>
+      }
+    </div>
+  )
+}
